3d369fdc6a25a64a51cab6cd591419a046209ac4
Niko Ware <wniko5482@gmail.com>
2022-07-24 07:20:37 -0400

    created logic/PATHS.py file to store content links

---
+++ logic/PATHS.py
@@ -0,0 +1,9 @@
+# keep content paths here as links
+# import in file by using `import PATHS`
+# reference with PATHS.<NAMEOFREFERENCE>
+
+CONTENT="../content/"
+APPLIANCES="applianceslist/applistoutput.csv"
+APPLIANCES2="applianceslist/applist.csv"
+PLACEHOLDER="appliancepics/circle1.png"
+
