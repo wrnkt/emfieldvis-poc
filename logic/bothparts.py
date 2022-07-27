29f656186eec12261fa1365e7902c3430526709e
Niko Ware <wniko5482@gmail.com>
2022-07-26 23:13:56 -0400

    cleaned up import statements, resolved namespace conflict, made Tk class references explicit.

--- logic/bothparts.py
+++ logic/bothparts.py
@@ -1,16 +1,19 @@
-from tkinter import *
-from PIL import ImageTk,Image
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas
 import os
 import csv
+
+from PIL import ImageTk,Image
+import PIL.Image
+
 import tkinter as tk
-from tkinter import filedialog
-from tkinter import simpledialog
+from tkinter import filedialog, simpledialog
+
 import matplotlib
-matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+matplotlib.use("TkAgg")
+
+import numpy as np
+import pandas
 
 from PATHS import APPLIST_OUTPUT_PATH, \
                     PLACEHOLDER_PATH, \
@@ -68,11 +71,11 @@ def createAppliance(applianceselection):
     placeholderimage = placeholderimage.resize((int(25*pixelspercm),int(25*pixelspercm)), Image.ANTIALIAS)
     i = ImageTk.PhotoImage(placeholderimage)
     #count = len(root.winfo_children())
-    currentapp = Label(leftFrame, text=applianceselection,background="#ffffe0",pady=0, padx=0, borderwidth=0, highlightthickness = 0)
+    currentapp = tk.Label(leftFrame, text=applianceselection,background="#ffffe0",pady=0, padx=0, borderwidth=0, highlightthickness = 0)
     currentapp["compound"] = tk.LEFT
     currentapp["image"] = i
     currentapp.image = i
-    currentapp.place(bordermode=INSIDE,x=w/2,y=h/2)
+    currentapp.place(bordermode=tk.INSIDE,x=w/2,y=h/2)
     appliances.append(currentapp)
     enableDrag(currentapp)
     #label = Label(leftFrame, text=applianceselection, justify=LEFT,background="#ffffe0", relief=SOLID, borderwidth=1,font=("tahoma", "12", "normal"))
@@ -137,11 +140,11 @@ columnnames = ['appliance', 'xcm', 'ycm','current','radius']
 appdata = pandas.read_csv((APPLIST_OUTPUT_PATH_2), names=columnnames)
 sourcenames = appdata.appliance.tolist()
 
-root = Tk()
+root = tk.Tk()
 root.title("Appliance Arranger")
 
-leftFrame = Frame(root)
-leftFrame.pack(side = LEFT)
+leftFrame = tk.Frame(root)
+leftFrame.pack(side = tk.LEFT)
 
 imagedir = filedialog.askopenfilename()
 
@@ -160,35 +163,35 @@ print(w)
 print(h)
 print(pixelspercm)
 
-piccanvas = Canvas(leftFrame, width = w, height = h) # set canvas size based on floorplan size
+piccanvas = tk.Canvas(leftFrame, width = w, height = h) # set canvas size based on floorplan size
 piccanvas.pack()
-piccanvas.create_image(0, 0, anchor=NW, image=img)
+piccanvas.create_image(0, 0, anchor=tk.NW, image=img)
 
-rightFrame = Frame(root, width = 500, height = h, highlightbackground="black", highlightthickness=2, background="light grey")
-rightFrame.pack(expand=True, fill=BOTH)
+rightFrame = tk.Frame(root, width = 500, height = h, highlightbackground="black", highlightthickness=2, background="light grey")
+rightFrame.pack(expand=True, fill=tk.BOTH)
 
-promptlabel = Label(rightFrame,text="Select an appliance: ")
+promptlabel = tk.Label(rightFrame,text="Select an appliance: ")
 promptlabel.grid(row=1, sticky="NESW")
 
-optionchoice = StringVar(rightFrame)
+optionchoice = tk.StringVar(rightFrame)
 optionchoice.set(sourcenames[0]) # default value
-dropdown = OptionMenu(rightFrame, optionchoice, *sourcenames)
+dropdown = tk.OptionMenu(rightFrame, optionchoice, *sourcenames)
 dropdown.config(width=20)
 dropdown.grid(row=2)
 
-placebutton = Button(rightFrame, command = lambda : createAppliance(optionchoice.get()), text="Place", width="10", height="5")
+placebutton = tk.Button(rightFrame, command = lambda : createAppliance(optionchoice.get()), text="Place", width="10", height="5")
 placebutton.grid(row=3, sticky="NEW")
 
-deletebutton = Button(rightFrame, command = deleteLast, text="Delete Last Appliance", width="10", height="5")
+deletebutton = tk.Button(rightFrame, command = deleteLast, text="Delete Last Appliance", width="10", height="5")
 deletebutton.grid(row=4, sticky="NEW")
 
-donebutton = Button(rightFrame, command = loadData, text="Confirm Arrangement", width="10", height="5")
+donebutton = tk.Button(rightFrame, command = loadData, text="Confirm Arrangement", width="10", height="5")
 donebutton.grid(row=5, sticky="NEW")
 
-exitbutton = Button(rightFrame, command = calculate, text="Calculate", width="10", height="5")
+exitbutton = tk.Button(rightFrame, command = calculate, text="Calculate", width="10", height="5")
 exitbutton.grid(row=6, sticky="NEW")
 
-closebutton = Button(rightFrame, command = quitprogram, text="Quit", width="10", height="5")
+closebutton = tk.Button(rightFrame, command = quitprogram, text="Quit", width="10", height="5")
 closebutton.grid(row=7, sticky="NEW")
 
 root.mainloop()
