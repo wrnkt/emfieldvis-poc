69536a9b4bc62ec55903e65d0449750b3af19728
Niko Ware <wniko5482@gmail.com>
2022-08-26 18:51:06 -0400

    Clean up directory for Python development

--- java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main
-{
-    public static void main(String[] args) {
-        MagFieldCalculator mfc = new MagFieldCalculator();
-        System.out.println("test " + mfc.radiusFromCurrentAndField(5.5,1.5)); //test
-    }
-}
