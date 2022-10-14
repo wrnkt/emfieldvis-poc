64cc4cca424aefe0a19ebf189fa056d5f3555018
Niko <wrnkt1@gmail.com>
2022-10-14 16:53:56 -0400

    Rename source folder to src

---
+++ src/PATHS.py
@@ -0,0 +1,12 @@
+# keep content paths here as links
+# import in logic file by adding
+# paths to the `from PATHS import...` line
+
+CONTENT="../content/"
+APPLIANCES="applianceslist/applistoutput.csv"
+APPLIANCES2="applianceslist/applist.csv"
+PLACEHOLDER="appliancepics/circle1.png"
+
+APPLIST_OUTPUT_PATH=CONTENT+APPLIANCES
+APPLIST_OUTPUT_PATH_2=CONTENT+APPLIANCES2
+PLACEHOLDER_PATH=CONTENT+PLACEHOLDER
